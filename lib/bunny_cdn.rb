421bbb2ef77dfb78bbf5286d41514af8ac5197e1
Brandon Meeks <brandonmeeks@Brandon-Meekss-MacBook-Pro.local>
2020-03-27 12:34:22 -0400

    Updated Configuration

--- lib/bunny_cdn.rb
+++ lib/bunny_cdn.rb
@@ -5,12 +5,4 @@ require_relative "bunny_cdn/configuration"
 require_relative "bunny_cdn/storage"
 
 module BunnyCdn
-    class << self
-        attr_accessor :configuration
-    end
-
-    def self.configure
-        self.configuration ||= Configuration.new
-        yield(configuration)
-    end
 end
--- lib/bunny_cdn/configuration.rb
+++ lib/bunny_cdn/configuration.rb
@@ -7,4 +7,17 @@ module BunnyCdn
             @accessKey = nil
         end
     end
+
+    def self.configuration
+        @configuration ||= Configuration.new
+    end
+
+    # Set BunnyCdn's configuration
+    def self.configuration=(config)
+        @configuration = config
+    end
+
+    def self.configure
+        yield(configuration)
+    end
 end
