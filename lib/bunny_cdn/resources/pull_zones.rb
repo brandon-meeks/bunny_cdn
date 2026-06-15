2e2c5edad5e76d5204e2b237d7431867757d4fcc
Ben D'Angelo <ben@bendangelo.me>
2026-06-14 21:10:22 -0400

    feat: add PullZones resource with all endpoint methods

---
+++ lib/bunny_cdn/resources/pull_zones.rb
@@ -0,0 +1,69 @@
+require "cgi"
+
+module BunnyCdn
+  module Resources
+    class PullZones < Base
+      def list
+        get("pullzone", { page: 1, perPage: 1 })
+      end
+
+      def find(id)
+        get("pullzone/#{id.to_i}")
+      end
+
+      def create(name:, type:, origin_url:)
+        post("pullzone", {
+          name: name,
+          type: type,
+          originUrl: origin_url
+        })
+      end
+
+      def delete(id)
+        path = "pullzone/#{id.to_i}"
+        response = connection.delete(path)
+        handle_response(response, path)
+      end
+
+      def purge(id)
+        post("pullzone/#{id.to_i}/purgeCache")
+      end
+
+      def add_hostname(id, hostname)
+        post("pullzone/#{id.to_i}/addHostname", {
+          "Hostname" => hostname
+        })
+      rescue ApiError => e
+        if hostname_already_registered?(e)
+          { "success" => true, "skipped" => true }
+        else
+          raise
+        end
+      end
+
+      def load_free_ssl(hostname:)
+        get("pullzone/loadFreeCertificate", { hostname: hostname })
+      end
+
+      def purge_by_tag(id, tag)
+        post("pullzone/#{id.to_i}/purgeCache?cacheTag=#{CGI.escape(tag)}", {})
+      end
+
+      def health_check
+        get("pullzone", { page: 1, perPage: 1 })
+        true
+      rescue ApiError
+        false
+      end
+
+      private
+
+      def hostname_already_registered?(error)
+        return false unless error.status == 400
+        return false unless error.response.is_a?(Hash)
+
+        error.response["ErrorKey"] == "pullzone.hostname_already_registered"
+      end
+    end
+  end
+end
---
+++ spec/resources/pull_zones_spec.rb
@@ -0,0 +1,188 @@
+require "spec_helper"
+
+RSpec.describe BunnyCdn::Resources::PullZones do
+  let(:client) { BunnyCdn::Client.new(api_key: "test-key") }
+  let(:resource) { described_class.new(client) }
+
+  describe "#list" do
+    before do
+      stub_request(:get, "https://api.bunny.net/pullzone?page=1&perPage=1")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: [{ "Id" => 1 }].to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "returns parsed JSON" do
+      result = resource.list
+      expect(result).to eq([{ "Id" => 1 }])
+    end
+  end
+
+  describe "#find" do
+    before do
+      stub_request(:get, "https://api.bunny.net/pullzone/12345")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: { "Id" => 12345 }.to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "returns the pull zone" do
+      result = resource.find(12345)
+      expect(result).to eq({ "Id" => 12345 })
+    end
+  end
+
+  describe "#create" do
+    before do
+      stub_request(:post, "https://api.bunny.net/pullzone")
+        .with(
+          headers: { "AccessKey" => "test-key", "Accept" => "application/json", "Content-Type" => "application/json" },
+          body: { name: "test", type: 0, originUrl: "http://example.com" }.to_json
+        )
+        .to_return(status: 200, body: { "Id" => 1 }.to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "creates a pull zone" do
+      result = resource.create(name: "test", type: 0, origin_url: "http://example.com")
+      expect(result).to eq({ "Id" => 1 })
+    end
+  end
+
+  describe "#delete" do
+    before do
+      stub_request(:delete, "https://api.bunny.net/pullzone/12345")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: "", headers: { "Content-Type" => "application/json" })
+    end
+
+    it "deletes the pull zone" do
+      result = resource.delete(12345)
+      expect(result).to eq("")
+    end
+  end
+
+  describe "#purge" do
+    before do
+      stub_request(:post, "https://api.bunny.net/pullzone/12345/purgeCache")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: "", headers: { "Content-Type" => "application/json" })
+    end
+
+    it "purges cache" do
+      result = resource.purge(12345)
+      expect(result).to eq("")
+    end
+  end
+
+  describe "#add_hostname" do
+    context "when hostname is not registered" do
+      before do
+        stub_request(:post, "https://api.bunny.net/pullzone/12345/addHostname")
+          .with(
+            headers: { "AccessKey" => "test-key", "Accept" => "application/json", "Content-Type" => "application/json" },
+            body: { "Hostname" => "cdn.example.com" }.to_json
+          )
+          .to_return(status: 200, body: { "Hostname" => "cdn.example.com" }.to_json, headers: { "Content-Type" => "application/json" })
+      end
+
+      it "adds the hostname" do
+        result = resource.add_hostname(12345, "cdn.example.com")
+        expect(result).to eq({ "Hostname" => "cdn.example.com" })
+      end
+    end
+
+    context "when hostname is already registered" do
+      before do
+        stub_request(:post, "https://api.bunny.net/pullzone/12345/addHostname")
+          .with(
+            headers: { "AccessKey" => "test-key", "Accept" => "application/json", "Content-Type" => "application/json" },
+            body: { "Hostname" => "cdn.example.com" }.to_json
+          )
+          .to_return(
+            status: 400,
+            body: { "ErrorKey" => "pullzone.hostname_already_registered" }.to_json,
+            headers: { "Content-Type" => "application/json" }
+          )
+      end
+
+      it "returns success with skipped flag" do
+        result = resource.add_hostname(12345, "cdn.example.com")
+        expect(result).to eq({ "success" => true, "skipped" => true })
+      end
+    end
+
+    context "when other errors occur" do
+      before do
+        stub_request(:post, "https://api.bunny.net/pullzone/12345/addHostname")
+          .to_return(status: 500, body: "internal error", headers: { "Content-Type" => "text/plain" })
+      end
+
+      it "raises ApiError" do
+        expect { resource.add_hostname(12345, "cdn.example.com") }.to raise_error(BunnyCdn::ApiError)
+      end
+    end
+  end
+
+  describe "#load_free_ssl" do
+    before do
+      stub_request(:get, "https://api.bunny.net/pullzone/loadFreeCertificate?hostname=cdn.example.com")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: { "Success" => true }.to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "loads free SSL certificate" do
+      result = resource.load_free_ssl(hostname: "cdn.example.com")
+      expect(result).to eq({ "Success" => true })
+    end
+  end
+
+  describe "#purge_by_tag" do
+    before do
+      stub_request(:post, "https://api.bunny.net/pullzone/12345/purgeCache?cacheTag=my-tag")
+        .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+        .to_return(status: 200, body: "", headers: { "Content-Type" => "application/json" })
+    end
+
+    it "purges cache by tag" do
+      result = resource.purge_by_tag(12345, "my-tag")
+      expect(result).to eq("")
+    end
+  end
+
+  describe "#health_check" do
+    context "when API is healthy" do
+      before do
+        stub_request(:get, "https://api.bunny.net/pullzone?page=1&perPage=1")
+          .with(headers: { "AccessKey" => "test-key", "Accept" => "application/json" })
+          .to_return(status: 200, body: [{ "Id" => 1 }].to_json, headers: { "Content-Type" => "application/json" })
+      end
+
+      it "returns true" do
+        expect(resource.health_check).to be true
+      end
+    end
+
+    context "when API is unhealthy" do
+      before do
+        stub_request(:get, "https://api.bunny.net/pullzone?page=1&perPage=1")
+          .to_return(status: 500, body: "error", headers: { "Content-Type" => "text/plain" })
+      end
+
+      it "returns false" do
+        expect(resource.health_check).to be false
+      end
+    end
+  end
+
+  describe "error handling" do
+    before do
+      stub_request(:get, "https://api.bunny.net/pullzone/99999")
+        .to_return(status: 404, body: { "error" => "not found" }.to_json, headers: { "Content-Type" => "application/json" })
+    end
+
+    it "raises ApiError with status and response" do
+      expect { resource.find(99999) }.to raise_error(BunnyCdn::ApiError) do |error|
+        expect(error.status).to eq(404)
+        expect(error.response).to eq({ "error" => "not found" })
+      end
+    end
+  end
+end
